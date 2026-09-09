7c0cc08935e1ab4294984eb93fb94a922790bf7f
ojas-mehla <ojas.41791@stu.upes.ac.in>
2026-09-09 16:27:13 +0530

    Add files via upload

---
+++ gethub.c
@@ -0,0 +1,26 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the value of a\n");
+scanf("%d",&a);
+printf("enter the value of b\n");
+scanf("%d",&b);
+printf("sum of a and b is %d\n",a+b);
+return 0;
+}
